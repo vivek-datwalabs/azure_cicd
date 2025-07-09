c9bdab7feac208f6e5116e288850e284cff31223
vivek-datwalabs <vivekranjan.sahoo@datwalabs.com>
2025-07-10 02:16:39 +0530

    latest code

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "FastAPI is running 🚀 in dev mode"}
+    return {"message": "FastAPI is running 🚀 in dev mode with latest code"}
