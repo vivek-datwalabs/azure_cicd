0207b03d4cc6930dc072881430260bdea948e139
vivek-datwalabs <vivekranjan.sahoo@datwalabs.com>
2025-07-10 02:32:46 +0530

    ready for prod

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "FastAPI is running 🚀 in dev mode with latest code"}
+    return {"message": "FastAPI is running 🚀 in dev mode with latest code ready to main now"}
