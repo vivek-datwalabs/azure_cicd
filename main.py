a15c9e25abcce9a443548b7ddffe354e6ba33567
vivek-datwalabs <vivekranjan.sahoo@datwalabs.com>
2025-07-10 07:17:25 +0530

    without restart

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "FastAPI is running 🚀 in dev mode with latest code ready to main now"}
+    return {"message": "FastAPI is running 🚀 in dev mode with latest code ready to main now without restart"}
