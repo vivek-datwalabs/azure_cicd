bf939213b276984d8f0d2c8b74aa7b2daa3a5d8a
vivek-datwalabs <vivekranjan.sahoo@datwalabs.com>
2025-07-10 16:59:24 +0530

    custom domain added

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "FastAPI is running 🚀 in dev mode with latest code ready to main now without restart app services final go !!!!"}
+    return {"message": "FastAPI is running 🚀 in dev mode with latest code ready to main now without restart app services final go with a custom domain telvi.vivekranjansahoo.in  !!!!"}
